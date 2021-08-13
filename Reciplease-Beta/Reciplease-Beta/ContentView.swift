e5f8d1bc074ec55e88394f17129f0acb76986dc9
SamRoyal <roysa235@student.otago.ac.nz>
2021-08-13 16:24:37 +1200

    Added more buttons to the homscreen, and 2 text fields
    Added more buttons to the home screen, and some text fields to input how many people are cooking and the budget

--- Reciplease-Beta/Reciplease-Beta/ContentView.swift
+++ Reciplease-Beta/Reciplease-Beta/ContentView.swift
@@ -9,14 +9,46 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var numCooking = ""
+    @State var budget = ""
+    
     var body: some View {
         VStack{
-            Spacer()
-            Text("RECIPLEASE").font(Font.custom("BebasNeue-Regular",size: 50))
-                .padding()
-            Text("You currently have 24 Recipe's stored").font(Font.custom("BebasNeue-Regular",size: 23))
-                .padding()
-            Buttons()
+            VStack{
+                Text("RECIPLEASE").font(Font.custom("BebasNeue-Regular",size: 50))
+                Text("You currently have 24 Recipe's stored").font(Font.custom("BebasNeue-Regular",size: 23))
+                    .padding(15)
+                AddRecipeButton()
+                    .scaledToFit()
+                    .padding()
+                Spacer()
+                
+                TextField("Number of people you are cooking for",text: $numCooking)
+                    .padding(10)
+                    .frame(width: UIScreen.main.bounds.width - 20)
+                    .background(Color(#colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1)))
+                    .clipShape(RoundedRectangle(cornerRadius: 16, style:
+                    .continuous))
+                    .foregroundColor(Color(#colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1)))
+                        
+                TextField("Budget",text: $budget)
+                    .padding(10)
+                    .frame(width: UIScreen.main.bounds.width - 20)
+                    .background(Color(#colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1)))
+                    .clipShape(RoundedRectangle(cornerRadius: 16, style:
+                    .continuous))
+                    .foregroundColor(Color(#colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1)))
+                
+                RecipeFinderButton()
+                    .padding(10)
+                    Spacer()
+                        .frame(height:100)
+                BrowseRecipesButton()
+                    .padding()
+            }
+            .frame(height: UIScreen.main.bounds.height - 50,alignment: .top)
+            
+
         }
         .frame(maxWidth: .infinity, maxHeight: .infinity)
         .background(Color(#colorLiteral(red: 1, green: 0.8612575531, blue: 0.6343607306, alpha: 1)))
