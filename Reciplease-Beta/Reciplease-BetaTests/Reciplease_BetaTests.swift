eca565aae4c274e12432112659d15e3c09698c0f
timcop <timothy.copland98@gmail.com>
2021-08-15 19:44:45 +1200

    Added simple API test

---
+++ Reciplease-Beta/Reciplease-BetaTests/Reciplease_BetaTests.swift
@@ -0,0 +1,23 @@
+//
+//  Reciplease_BetaTests.swift
+//  Reciplease-BetaTests
+//
+//  Created by Tim Copland on 15/08/21.
+//  Copyright © 2021 Samuel Royal. All rights reserved.
+//
+
+import XCTest
+@testable import Reciplease_Beta
+
+class Reciplease_BetaTests: XCTestCase {
+    
+    func testWebScrape() {
+        Data.fillProds()
+        print("HEREEEEEEEE")
+        print(Data.Ingredients)
+    }
+    
+    // test adding recipe
+    
+    
+}
