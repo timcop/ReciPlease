8bf4f2d5698e0c9cccf91c79297c0f484cef56fb
timcop <timothy.copland98@gmail.com>
2021-10-06 19:09:26 +1300

    Loading screen

--- ReciPlease/ReciPlease/ReciPleaseApp.swift
+++ ReciPlease/ReciPlease/ReciPleaseApp.swift
@@ -18,7 +18,12 @@ struct ReciPleaseApp: App {
         WindowGroup {
             Group {
                if (isLoaded) { ContentView().environmentObject(recipeModel) }
-               else { Text("Loading . . .") }
+               else {
+                   
+//                   Text("Loading . . .")
+                   LoadingView()
+                   
+               }
             }.onAppear() {
                 if recipeModel.recipes.count == 1 {
                     recipeModel.recipes = recipeModel.getRecList()
@@ -49,4 +54,27 @@ func completionHandler(value: @escaping (Bool) -> Void) {
     
     DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + DispatchTimeInterval.milliseconds(3000)) { value(true) }
     
+}
+
+struct LoadingView: View {
+    @State private var isLoading = false
+    var body: some View {
+        ZStack {
+            Text("Cooking up your Recipes")
+                .font(.system(.body, design: .rounded))
+                .bold()
+                .offset(x: 0, y: -25)
+            RoundedRectangle(cornerRadius: 3)
+                .stroke(Color(.systemGray5), lineWidth: 3)
+                .frame(width: 250, height: 3)
+            RoundedRectangle(cornerRadius: 3)
+                .stroke(Color.orange, lineWidth: 3)
+                .frame(width: 30, height: 3)
+                .offset(x: 110, y: 0)
+//                .animation(Animation.linear(duration: 1).repeatForever(autoreverses: false))
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(Color(#colorLiteral(red: 1, green: 0.8612575531, blue: 0.6343607306, alpha: 1)))
+        .edgesIgnoringSafeArea(.all)
+    }
 }
